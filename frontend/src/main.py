6aedd131a5f1a31efcebd7b8425bbedc2a2ee8d2
daisukeokamoto1023 <okadaih11023@gmail.com>
2021-01-17 19:13:19 +0900

    create login page

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,19 +1,40 @@
 import requests
 import streamlit as st
 import os
+import logging
 from os.path import join, dirname
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
-
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 BACKEND_URL = os.environ.get("BACKEND_URL")
 
-st.title("Welcome APP!")
 
-st.write(BACKEND_URL)
+def is_authenticated(id: str, password: str) -> bool:
+    temp_id = "hoge"
+    temp_pass = "abc123"
+    return temp_id == id and temp_pass == password
 
-if st.button("Style Transfer"):
-    res = requests.get(BACKEND_URL)
-    st.write(res.text)
+
+def main_screen():
+
+    if st.button("Check Backend Connection"):
+        res = requests.get(BACKEND_URL)
+        st.write(res.text)
+
+
+if __name__ == "__main__":
+    # Password Form
+    st.title("Welcome APP!")
+    logging.info(f"backend_url is {BACKEND_URL}")
+    id_block = st.empty()
+    password_block = st.empty()
+    id = id_block.text_input('Id')
+    password = password_block.text_input("Enter a password", type="password")
+    if is_authenticated(id, password):
+        id_block.empty()
+        password_block.empty()
+        main_screen()
+    elif password:
+        st.info("Please enter a valid password")
