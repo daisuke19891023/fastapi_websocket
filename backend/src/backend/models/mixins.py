f7262907c1e032d5323d62190fa202cb34d3ee59
daisukeokamoto1023 <okadaih11023@gmail.com>
2021-02-04 00:11:35 +0900

    use envfile for test and production

--- backend/src/backend/models/mixins.py
+++ backend/src/backend/models/mixins.py
@@ -2,10 +2,18 @@
 
 from sqlalchemy import Column, text
 from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+dotenv_path = os.path.join(os.path.abspath(__file__), '.env')
+load_dotenv(dotenv_path)
+MODE = os.environ.get('MODE')
 
 
 class TimestampMixin(object):
     created_at = Column(Timestamp, nullable=False,
                         server_default=text('current_timestamp'))
-    # updated_at = Column(Timestamp, nullable=False,
-    #                     server_default=text('current_timestamp on update current_timestamp'))
+    # os.environ["IS_TEST"] = "hoge"
+    mysql_logic = 'current_timestamp on update current_timestamp' if MODE != "TEST" else 'current_timestamp'
+    updated_at = Column(Timestamp, nullable=False,
+                        server_default=text(mysql_logic))
